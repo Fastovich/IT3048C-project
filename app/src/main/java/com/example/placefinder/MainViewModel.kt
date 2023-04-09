7d8de486ccf167c25a267969cacb9d1598face70
fastovich <lfastovich@gmail.com>
2023-04-09 19:32:56 -0400

    commit (4/6/2023) Lucas Fastovich - Added Firebase and firestore to project. - Current Implementation of Firebase and firestore for User and user sign in -(no implementation of buttons yet or dropdown menus.)

--- app/src/main/java/com/example/placefinder/MainViewModel.kt
+++ app/src/main/java/com/example/placefinder/MainViewModel.kt
@@ -1,22 +1,44 @@
 package com.example.placefinder
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.placefinder.dto.Park
+import com.example.placefinder.dto.User
 import com.example.placefinder.service.IParkService
 import com.example.placefinder.service.ParkService
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
     var parks = MutableLiveData<Park.Datum.Address>()
     var parkService : IParkService = ParkService()
+    var user : User? = null
 
+    private lateinit var firestore : FirebaseFirestore
+
+    init {
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+    }
     fun fetchParks() {
         viewModelScope.launch {
             var innerPark = parkService.fetchParks()
             parks.postValue(innerPark)
         }
     }
+ //setting up database for park info storage currently still implementing.
+    fun savePark(parks: Park){
 
+    }
+    fun saveUser() {
+        user?.let{
+            user ->
+            var handle = firestore.collection("users").document(user.uid).set(user)
+            handle.addOnSuccessListener { Log.d("Firebase", "User saved successful") }
+            handle.addOnFailureListener {Log.e("Firebase", "User save failed")}
+        }
+    }
 }
